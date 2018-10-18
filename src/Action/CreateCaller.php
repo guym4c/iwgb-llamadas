075a0876e0070145ea76f457a7b467c13af4f64f
Guy Mac <hello@guymac.eu>
2018-10-18 16:37:48 +0100

    Email new users

--- src/Action/CreateCaller.php
+++ src/Action/CreateCaller.php
@@ -7,7 +7,8 @@ use Psr\Http\Message\ResponseInterface as Response;
 
 class CreateCaller extends CampaignAction {
 
-    const NOTIFICATION_EMAIL_SUBJECT = 'New IWGB Llamadas User';
+    const ADMIN_EMAIL_SUBJECT = 'New IWGB Llamadas User';
+    const NOTIFICATION_EMAIL_SUBJECT = 'Welcome to IWGB Llamadas';
 
     public function __invoke(Request $request, Response $response, $args) {
         $post = $request->getParsedBody();
@@ -16,11 +17,18 @@ class CreateCaller extends CampaignAction {
         }
         \Caller::constructCaller($this->db, $post['email'], $post['name'], $args['campaign'])->save();
         (new \Email(\Keys::Admin,
-            self::NOTIFICATION_EMAIL_SUBJECT,
+            self::ADMIN_EMAIL_SUBJECT,
             "New IWGB Llamadas user has agreed to the Data Protection Statement.\n\n
             Name: " . $post['name'] . "\n
             Email: " . $post['email'] . "\n
             Campaign: " . $args['campaign']
+        ))->send();
+        ( new \Email($post['email'],
+            self::NOTIFICATION_EMAIL_SUBJECT,
+            'Welcome to IWGB Llamadas, ' . $post['name'] . "!\n\n
+            You're on the " . $args['campaign'] . " project - you can log in here:\n
+            https://llamadas.iwgb.org.uk/call/" . $args['campaign'] . "\n
+            To get your campaign password, speak to your IWGB Rep."
         ))->send();
         return $response->withRedirect('/call/' . $args['campaign']);
 
