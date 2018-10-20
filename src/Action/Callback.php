49f3639bf07ef9068cd5316903441903a7003763
Guy Mac <hello@guymac.eu>
2018-10-20 14:04:19 +0200

    Ability to block 500 retries

--- src/Action/Callback.php
+++ src/Action/Callback.php
@@ -7,8 +7,12 @@ use Psr\Http\Message\ResponseInterface as Response;
 
 class Callback extends GenericAction {
 
+    const BLOCKED_EVENTS = [];
+
     public function __invoke(Request $request, Response $response, $args) {
         $json = $request->getParsedBody();
+        if (in_array($json['event_id'], self::BLOCKED_EVENTS)) return $response->withStatus(200);
+
         if ($json['event_type'] != 'form_response') return $this->notFound($request, $response);
         $params = $json['form_response']['hidden'];
         $answers = $json['form_response']['answers'];
